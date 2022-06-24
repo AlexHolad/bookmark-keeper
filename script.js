dd51ad91289124d0aef55443969f5affbb6eebf6
alexholad <alexej.holad@gmail.com>
2022-06-24 14:19:30 +0200

    built bookmarks DOM

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContaner = document.getElementById("bookmarks-container");
 
-
 let bookmarks = [];
 
 // Show Modal, Focus on Input
@@ -41,36 +40,65 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
-
-// Fetch Bookmarks 
-function fetchBookmarks() {
-    // Get bookmarks from localStorage if available
-    if(localStorage.getItem('bookmarks')) {
-        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    } else {
-        // Create bookmarks array in localStorage
-        bookmarks = [
-            {
-                name: 'First Bookmark',
-                url: 'https://beebom.com/cool-interesting-websites/',
-            }
-        ];
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    }
-    console.log(bookmarks)
+// Build Bookmarks DOM
+function buildBookmarks() {
+    // Build items
+    bookmarks.forEach((bookmark) => {
+        const {name, url} = bookmark;
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item')
+        // Close Icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas', 'fa-times');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        // Favicon /Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name')
+        // Favicon  
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt', 'Favicon');
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`)
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContaner.appendChild(item);
+    });
 }
 
-
+// Fetch Bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "First Bookmark",
+        url: "https://beebom.com/cool-interesting-websites/",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
 
 // Handle Data from Form
 function storeBookmark(e) {
-    e.preventDefault();
+  e.preventDefault();
   const nameValue = websiteNameEl.value;
   let urlValue = websiteUrlEl.value;
   if (!urlValue.includes("https://") && !urlValue.includes("http://")) {
     urlValue = `https://${urlValue}`;
   }
-  if(!validate(nameValue, urlValue)) {
+  if (!validate(nameValue, urlValue)) {
     return false;
   }
   const bookmark = {
@@ -78,14 +106,14 @@ function storeBookmark(e) {
     url: urlValue,
   };
   bookmarks.push(bookmark);
-  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   fetchBookmarks();
   bookmarkForm.reset();
   websiteNameEl.focus();
 }
 
 // Event Listener
-bookmarkForm.addEventListener('submit', storeBookmark);
+bookmarkForm.addEventListener("submit", storeBookmark);
 
 // On Load, Fetch Bookmarks
 fetchBookmarks();
